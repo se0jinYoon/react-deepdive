96057dbb16ef233e9ce6b6067daa6b4ed44667be
se0jinYoon <tjwls1999@gmail.com>
2023-08-10 21:58:58 +0900

    error-handling

--- star-wars-project/src/App.js
+++ star-wars-project/src/App.js
@@ -6,21 +6,47 @@ import './App.css';
 function App() {
   const [movies, setMovies] = useState([]);
   const [isLoading, setIsLoading] = useState(false);
+  const [error, setError] = useState(null);
 
   async function fetchMoviesHandler() {
     setIsLoading(true);
-    const response = await fetch('https://swapi.dev/api/films/')
-    const data = await response.json();
-    const transformedMovies = data.results.map((movieData) => {
-      return {
-        id: movieData.episode_id,
-        title: movieData.title,
-        openingText: movieData.opening_crawl,
-        releaseDate: movieData.release_date,
-      };
-    });
-    setMovies(transformedMovies);
-    setIsLoading(false);
+    setError(null);
+    try {
+      const response = await fetch('https://swapi.dev/api/films/');
+      // 만약 여기서 에러가 발생해서 if문이 실행된다면
+      if (!response.ok) {
+        throw new Error('Something went wrong!');
+      }
+
+      // 아래 코드가 실행되지 않고
+      const data = await response.json();
+      const transformedMovies = data.results.map((movieData) => {
+        return {
+          id: movieData.episode_id,
+          title: movieData.title,
+          openingText: movieData.opening_crawl,
+          releaseDate: movieData.release_date,
+        };
+      });
+      setMovies(transformedMovies);
+      setIsLoading(false);
+      // catch문으로 이동하게된다
+      // 여기서 받아오는 error는 state값이 아닌 new Error로 만든 객체값이다
+    } catch (error) {
+      setError(error.message);
+    }
+    setIsLoading(false); 
+  }
+
+  let content = <p>Found no movies.</p>;
+  if (movies.length > 0) {
+    content = <MoviesList movies={movies} />;
+  }
+  if (error) {
+    content = <p>{error}</p>
+  }
+  if (isLoading) {
+    content = <p>Loading...</p>
   }
 
   return (
@@ -29,9 +55,7 @@ function App() {
         <button onClick={fetchMoviesHandler}>Fetch Movies</button>
       </section>
       <section>
-        {!isLoading && movies.length > 0 && <MoviesList movies={movies} />}
-        {!isLoading && movies.length === 0 && <p>Found no movies.</p>}
-        {isLoading && <p>Loading...</p>}
+        {content}
       </section>
     </React.Fragment>
   );
