17a05823baff8a0d37a1a219aac41f2915c38b14
se0jinYoon <tjwls1999@gmail.com>
2023-08-20 19:21:17 +0900

    customHoom 직접 구현

--- form/src/hooks/use-basicInput.js
+++ form/src/hooks/use-basicInput.js
@@ -4,7 +4,29 @@ const useBasicInput = (validateValue) => {
     const [value, setValue] = useState('');
     const [valueIsTouched, setValueIsTouched] = useState(false);
 
-    
+    const valueIsValid = validateValue(value);
+    const hasError = !valueIsValid && valueIsTouched;
+
+    const onChangeHandler = e => {
+        setValue(e.target.value);
+    }
+    const onBlurHandler = e => {
+        setValueIsTouched(true);
+    }
+
+    const reset = () => {
+        setValue('');
+        setValueIsTouched(false);
+    }
+
+    return {
+        value,
+        valueIsValid,
+        hasError,
+        onChangeHandler,
+        onBlurHandler,
+        reset
+    }
 };
 
 export default useBasicInput;
