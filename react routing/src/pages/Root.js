72562722f3c12f271cfcb46a4a15fc0368e2bcfa
se0jinYoon <tjwls1999@gmail.com>
2023-08-29 13:41:53 +0900

    link nav bar

---
+++ react routing/src/pages/Root.js
@@ -0,0 +1,17 @@
+// outlet은 chlidren들이 렌더링되어야할 위치를 정해준다
+import { Outlet } from 'react-router-dom';
+import MainNavigation from '../components/MainNavaigation';
+import classes from './Root.module.css'
+// child route 요소들이 어느 위치에 렌더링 되어야할지 정하는 부모 컴포넌트
+function RootLayout() {
+  return (
+    <>
+      <MainNavigation />
+      <main className={classes.content}>
+      <Outlet />
+      </main>
+    </>
+  );
+}
+
+export default RootLayout;
